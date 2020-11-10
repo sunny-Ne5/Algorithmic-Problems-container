5a1f1d71b5173124c4102a1cdee26a8949e5b8d5
sunny-Ne5 <sunny.tiwari505@gmail.com>
2020-11-11 01:22:50 +0530

    Leetcode Practice

--- leetcode/practice/75sort-colors.cpp
+++ leetcode/practice/75sort-colors.cpp
@@ -6,11 +6,39 @@ Problem: https://leetcode.com/problems/sort-colors/
 */
 
 
+/*
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+*/
+
 
 
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        
-    }
+	void sortColors(vector<int>& nums) {
+		//algorithm
+		//arrange all the zeros to the left and two's to the right
+		//don't worry about the middle ones
+
+		int start = 0, end = n - 1;
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] == 0)
+			{
+				swap(nums[i], nums[start]);
+				start++;
+			}
+		}
+		int start = 0, end = n - 1;
+		for (int i = 0; i < n; i++)
+		{
+			if (nums[i] == 2)
+			{
+				swap(nums[i], nums[start]);
+				end--;
+			}
+		}
+
+	}
 };
