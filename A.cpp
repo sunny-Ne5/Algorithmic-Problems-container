7b43a077df67d24326e8903e8e56ca316a1c26f7
sunny-Ne5 <sunny.tiwari505@gmail.com>
2020-11-14 11:33:48 +0530

    Update

---
+++ A.cpp
@@ -0,0 +1,55 @@
+/*
+@author: Sunny Tiwari
+time: 20:6
+date: 14 November 2020 (Saturday)
+Problem:
+*/
+
+
+
+
+#include                            <iostream>
+#include                            <list>
+#include                            <stdio.h>
+#include                            <math.h>
+#include                            <string.h>
+#include                            <time.h>
+#include                            <stdlib.h>
+#include                            <string>
+#include                            <bitset>
+#include                            <vector>
+#include                            <set>
+#include                            <map>
+#include                            <queue>
+#include                            <algorithm>
+#include                            <sstream>
+#include                            <stack>
+#include                            <iomanip>
+#include                            <numeric>
+#include                            <unordered_map>
+#include                            <unordered_set>
+using namespace                     std;
+#define                             int long long int
+#define                             MOD 1000000007
+
+void solve()
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; i++)
+        cout << 1 << " ";
+    cout << endl;
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1; cin >> t;
+    for (int test = 1; test <= t; test++)
+    {
+
+        solve();
+    }
+    return 0;
+}
